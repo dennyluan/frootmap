fca347f64dc68b44b793f249b826eb34dbcb6008
Denny Luan <dennyluan@gmail.com>
2021-01-15 01:51:49 -1000

    Set up map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './assets/App.css';
 
+import GoogleMapReact from 'google-map-react';
+
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+
 function App() {
+
+  const props = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Frootmap</h1>
+      </header>
+
+      <body>
+        <p>Map</p>
+        <GoogleMapReact
+          // bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
+          defaultCenter={props.center}
+          defaultZoom={props.zoom}
+        >
+        </GoogleMapReact>
+      </body>
     </div>
   );
 }
